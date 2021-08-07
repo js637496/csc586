49cb33f3313fd3b6e8e70102c8d700c8d81baa6c
Jim Speers <jimspeers.speers@gmail.com>
2021-08-07 18:11:48 -0400

    Create record.sh

---
+++ record.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+NFS_PATH="/nfs/recordings"
+CAMERA_ID=$2
+
+year=`date +'%Y'`
+month=`date +'%m'`
+day=`date +'%d'`
+hour=`date +'%H'`
+minute=`date +'%M'`
+
+FULL_PATH=$NFS_PATH/$2/$year/$month/$day/$hour
+
+mkdir -p $NFS_PATH/$year/$month/$day/$hour
+
+ffmpeg -i $1 -t 60 -c copy -bsf:a aac_adtstoasc $FULL_PATH/$minute.mp4
