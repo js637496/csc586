bea909151fd37d00fd80221e6ae2cddbc95c8210
Jim Speers <jimspeers.speers@gmail.com>
2021-08-07 18:29:25 -0400

    Update record.sh

--- record.sh
+++ record.sh
@@ -11,6 +11,6 @@ minute=`date +'%M'`
 
 FULL_PATH=$NFS_PATH/$2/$year/$month/$day/$hour
 
-mkdir -p $NFS_PATH/$year/$month/$day/$hour
+mkdir -p $FULL_PATH
 
 ffmpeg -i $1 -t 60 -c copy -bsf:a aac_adtstoasc $FULL_PATH/$minute.mp4
