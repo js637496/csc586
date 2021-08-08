3b7d11280254dc4edbfdbf52d6cda9f36ff0ece7
Jim Speers <jimspeers.speers@gmail.com>
2021-08-07 23:21:30 -0400

    Update record.sh

--- record.sh
+++ record.sh
@@ -2,6 +2,7 @@
 
 NFS_PATH="/nfs/recordings"
 CAMERA_ID=$2
+STREAM_URL=$1
 
 year=`date +'%Y'`
 month=`date +'%m'`
@@ -9,8 +10,12 @@ day=`date +'%d'`
 hour=`date +'%H'`
 minute=`date +'%M'`
 
-FULL_PATH=$NFS_PATH/$2/$year/$month/$day/$hour
+#tar ball every hour for every camera (full daily is too large)
+FULL_PATH=$NFS_PATH/$year/$month/$day/$hour/$CAMERA_ID
 
 mkdir -p $FULL_PATH
 
-ffmpeg -i $1 -t 60 -c copy -bsf:a aac_adtstoasc $FULL_PATH/$minute.mp4
+ffmpeg -i $STREAM_URL -t 61 -c copy -bsf:a aac_adtstoasc $FULL_PATH/$minute.mp4
+
+#LATER - if its a new hours (00.mp4) start the tarball process for this camera's pervious hour
+#send to bucket.
