5a7b908c35bf99085e45362e8c554163429d971a
Jim Speers <jimspeers.speers@gmail.com>
2021-08-10 20:27:46 -0400

    Update record.sh

--- record.sh
+++ record.sh
@@ -10,7 +10,6 @@ day=`date +'%d'`
 hour=`date +'%H'`
 minute=`date +'%M'`
 
-#tar ball every hour for every camera (full daily is too large)
 FULL_PATH=$NFS_PATH/$year/$month/$day/$hour/$CAMERA_ID
 FULL_PATH=${FULL_PATH}_${minute}_.mp4
 
@@ -19,3 +18,4 @@ mkdir -p $FULL_PATH
 ffmpeg -i $STREAM_URL -t 61 -c copy -bsf:a aac_adtstoasc $FULL_PATH
 
 aws s3 mv $FULL_PATH s3://$S3BUCKET/$FULL_PATH
+#NFS isnt needed really as everything is going to S3
