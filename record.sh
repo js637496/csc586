6a5d6eefab150d5be45894ea1ee36abd7dc54d28
Jim Speers <jimspeers.speers@gmail.com>
2021-08-11 15:48:42 -0400

    Update record.sh

--- record.sh
+++ record.sh
@@ -11,10 +11,11 @@ hour=`date +'%H'`
 minute=`date +'%M'`
 
 HOUR_PATH=$year/$month/$day/$hour
-mkdir -p $NFS_PATH/$HOUR_PATH
-FULL_PATH=${HOUR_PATH}/${CAMERA_ID}_${minute}.mp4
+mkdir -p $NFS_PATH/$HOUR_PATH/$CAMERA_ID
+FULL_PATH=${HOUR_PATH}/${CAMERA_ID}/${minute}.mp4
 
-ffmpeg -i $STREAM_URL -t 61 -c copy -bsf:a aac_adtstoasc ${NFS_PATH}/$FULL_PATH
+ffmpeg -i $STREAM_URL -t 61 -filter:v fps=fps=8 ${NFS_PATH}/$FULL_PATH
 
-sudo aws s3 mv ${NFS_PATH}/$FULL_PATH s3://$S3BUCKET/$FULL_PATH
+#when min == 00, zip up the previous hour and push to glacier bucket
+#sudo aws s3 mv ${NFS_PATH}/$FULL_PATH s3://$S3BUCKET/$FULL_PATH
 #NFS isnt needed really as everything is going to S3
