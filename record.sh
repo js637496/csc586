10d07393b7a52a4ab7b364a5959a2f3147991eaf
Jim Speers <jimspeers.speers@gmail.com>
2021-08-10 21:43:35 -0400

    Update record.sh

--- record.sh
+++ record.sh
@@ -10,11 +10,11 @@ day=`date +'%d'`
 hour=`date +'%H'`
 minute=`date +'%M'`
 
-HOUR_PATH=$NFS_PATH/$year/$month/$day/$hour
-mkdir -p $HOUR_PATH
+HOUR_PATH=$year/$month/$day/$hour
+mkdir -p $NFS_PATH/$HOUR_PATH
 FULL_PATH=${HOUR_PATH}/${CAMERA_ID}_${minute}.mp4
 
-ffmpeg -i $STREAM_URL -t 61 -c copy -bsf:a aac_adtstoasc $FULL_PATH
+ffmpeg -i $STREAM_URL -t 61 -c copy -bsf:a aac_adtstoasc ${NFS_PATH}/$FULL_PATH
 
-sudo aws s3 mv $FULL_PATH s3://$S3BUCKET/$FULL_PATH
+sudo aws s3 mv ${NFS_PATH}/$FULL_PATH s3://$S3BUCKET/$FULL_PATH
 #NFS isnt needed really as everything is going to S3
